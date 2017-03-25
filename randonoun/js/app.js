2c4ef26c89cb0ab4738700e6a4afa2d25619655d
Joyce Lam <joyce@joystudio.ca>
2017-03-25 00:27:58 -0700

    workaround for constructing "haikus"

--- randonoun/js/app.js
+++ randonoun/js/app.js
@@ -5,39 +5,65 @@ var dbRef = new Firebase("https://nounrandomizer.firebaseio.com/");
 //set reference for nouns array (it's a child of the db tree)
 var nounsRef = dbRef.child('nouns');
 
-
 var randNum;
+var haiku = '';
 
-// I used the "value" event here instead of child_added since content is static
-// Doc: https://firebase.google.com/docs/database/admin/retrieve-data#value
-nounsRef.on("value", function(childdata){
-	//I could use .numChildren() to figure out how many noun objects I have in my entire DB
-	// Doc: https://firebase.google.com/docs/reference/js/firebase.database.DataSnapshot#numChildren
-	randNum = Math.floor(Math.random() * childdata.numChildren());
-	console.log( "total noun objs: " + childdata.numChildren(), "// randNum: " + randNum);
+
+//main function to generate noun/word from db
+function getNoun(branchRef){
 	
-	//generate randNoun object
-	var randNoun = childdata.child(randNum).val();
-	console.log(randNoun);	
-	
-	//convert randNoun object into an array using the each() function we used before in our json class
 	var nouns = [];
-	$.each(randNoun, function(key,value) {
-	    //pushing the values into the array
-	    nouns.push(value);
+	
+	//used .once() here since no new data is going to be updated
+	branchRef.once("value", function(childdata){				
+				
+		randNum = Math.floor(Math.random() * childdata.numChildren());
+		console.log( "total noun objs: " + childdata.numChildren(), "// randNum: " + randNum);
+		
+		//generate randNoun object
+		var randNoun = childdata.child(randNum).val();
+		console.log(randNoun);	
+		
+		//convert randNoun object into an array using the each() function we used before in our json class				
+		$.each(randNoun, function(key,value) {
+		    //pushing the values into the array
+		    nouns.push(value);
+		});
+		
+		choseNoun = chooseNoun(nouns);
+		console.log('choseNoun:' + choseNoun);
+		
+		addtoHaiku(choseNoun);
 	});
-	
-	//choose random noun from the nouns list
+}
+
+//choose random noun from the nouns list
+//separated this out to it's own function
+function chooseNoun(nounsArray){
 	//in our case, it's either singular/plural, but you can add more because we use .length
-	var r = Math.floor(Math.random() * nouns.length); 
-	var noun = nouns[r];
+	var r = Math.floor(Math.random() * nounsArray.length); 
+	noun = nounsArray[r];
 	
-	console.log( 'random noun array: ' + nouns , '// random noun chosen from array: ' + noun);
-	
-	//append our noun to page
-	$('#content').append( noun );
-});
+	console.log( 'random noun array: ' + nounsArray , '// random noun chosen from array: ' + noun);
+	return noun;
+}
 
+//add randomized word to the haiku string
+function addtoHaiku(word){
+	haiku += word + ' ';
+	printHaiku();
+}
 
+//print haiku on page
+function printHaiku(){
+	$('#content').html( haiku );
+}
 
+//construct haiku 
+function makeHaiku(){
+	getNoun(nounsRef);
+	getNoun(nounsRef);
+	getNoun(nounsRef);
+}
 
+makeHaiku();
